850f2ec9ab5d200f1751e432e8ac71778812a33c
keyth202 <combs.cdi@gmail.com>
2017-07-27 20:05:50 -0400

    Deployment Sequence

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const queryString = require('query-string');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const {PORT, DATABASE_URL} = require('./config');
+
+const app = express();
+
+app.use(express.static('public'));
+
+
+let server;
+
+// this function connects to our database, then starts the server
+function runServer(databaseUrl=DATABASE_URL, port=PORT) {
+
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+// this function closes the server, and returns a promise. we'll
+// use it in our integration tests later.
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+     return new Promise((resolve, reject) => {
+       console.log('Closing server');
+       server.close(err => {
+           if (err) {
+               return reject(err);
+           }
+           resolve();
+       });
+     });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
---
+++ test/test.js
@@ -0,0 +1,23 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const faker = require('faker');
+const mongoose = require('mongoose');
+
+const {app, runServer, closeServer} = require('../server');
+const {DATABASE_URL} = require('../config');
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Intial Test', function(){
+	it('should return a 200', function(){
+		return chai.request(app)
+			.get('/index')
+			.then(function(_res){
+				res = _res;
+				res.should.have.status(200);
+			});
+
+	});
+});
